e3732247b89362c0ecce37da3eac3ca9bd3c637d
201920288 (20192088) <143827370+201920288@users.noreply.github.com>
2023-09-05 07:53:21 +0900

    프로젝트 마무리 업로드 준비 완료

---
+++ src/subject2/script/reference.js
@@ -0,0 +1,59 @@
+const reference = [
+  {
+    author: "김한결",
+    title: "우리 갯벌의 생태계서비스 가치, 연간 17조8121억에 달해",
+    year: "2021",
+    sourceTitle: "",
+    link: "https://m.ecomedia.co.kr/news/newsview.php?ncode=1065575929756519",
+  },
+  {
+    author: "JOISS 관할해역 해양정보 공동활용 시스템",
+    title: "",
+    year: "",
+    sourceTitle: "",
+    link: "https://joiss.kr/joiss/main.do",
+  },
+  {
+    author: "OHI 해양건강지수",
+    title: "",
+    year: "",
+    sourceTitle: "",
+    link: "https://oceanhealthindex.org/",
+  },
+  {
+    author: "해양 환경 공단",
+    title: "국가 해양 생태계 종합조사-갯벌",
+    year: "2021",
+    sourceTitle: "습지보호지역 지정 주민 홍보 정도",
+    link: "https://www.youtube.com/watch?v=pBy1zgt0XPc",
+  },
+  {
+    author: "해양환경공단 해양보호구역관리",
+    title: "",
+    year: "",
+    sourceTitle: "",
+    link: "https://www.koem.or.kr/site/koem/04/10401040000002019051004.jsp ",
+  },
+  {
+    author: "홍현정",
+    title: "생태계 건강성 평가 기법 개발 및 활용 방안 연구",
+    year: "",
+    sourceTitle: "",
+    link: "",
+  },
+
+  {
+    author: "한국해양과학기술원",
+    title: "해양생태계 건강성 관련 국제동향",
+    year: "",
+    sourceTitle: "",
+    link: "",
+  },
+  {
+    author: "한국의 갯벌 세계유산 등재추진단",
+    title: "",
+    year: "",
+    sourceTitle: "",
+    link: "http://ktidalflats-heritage.com/home/",
+  },
+];
